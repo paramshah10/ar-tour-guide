c5370dece1c594975b2021fb0376ea18192fcd78
Suraj Vathsa <surajvathsa@gmail.com>
2020-01-26 18:04:59 -0800

    AR app framework created

---
+++ AR Tour Guide/AR Tour Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR Tour Guide
+//
+//  Created by iosDev on 1/26/20.
+//  Copyright © 2020 Creative Labs. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
